3084e8fbb0cb1e6fdb959c6d87b15c0a8fb4da97
GIRI8520 <cenationgiri143@gmail.com>
2026-09-13 23:00:24 +0530

    Add TypingIndicator component

---
+++ src/components/TypingIndicator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TypingIndicator: React.FC = () => {
+  return (
+    <div className="flex items-center space-x-2 p-2">
+      <span className="text-sm text-gray-400">Typing...</span>
+    </div>
+  );
+};

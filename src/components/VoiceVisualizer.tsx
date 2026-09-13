00f92c26c325e460a1590d8540696edd04f1e0e8
GIRI8520 <cenationgiri143@gmail.com>
2026-09-13 22:54:57 +0530

    Add VoiceVisualizer component

---
+++ src/components/VoiceVisualizer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const VoiceVisualizer: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center p-4">
+      <span className="text-sm text-gray-500">Voice Visualizer Active</span>
+    </div>
+  );
+};
